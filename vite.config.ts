6bf0bec3042a2dc97586e506dd8f1602d763046c
Franco Di Leo <dileo.francoj@gmail.com>
2024-04-23 05:29:06 -0300

    chore: add vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ outDir: 'dist', include: ["src/index.ts"], exclude: ["src/**/__tests__/**"] })],
+
   test: {
     globals: true,
     environment: 'jsdom',
     css: true,
     setupFiles: './tests/setup.ts'
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'lvlup-components',
+      formats: ['es', 'umd']
+    }
   }
 })
+
